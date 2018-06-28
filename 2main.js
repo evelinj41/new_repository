fd1c1731a89c07dd2d7eaec12d0ae2fd7b2fa4ee
evelinj41 <39770138+evelinj41@users.noreply.github.com>
2018-06-27 19:53:15 -0500

    condicionales

---
+++ 2main.js
@@ -0,0 +1,22 @@
+var fechaNacimiento = 1999;
+var fechaActual = 2018;
+
+if (fechaNacimiento <= fechaActual) {
+ var edad = fechaActual - fechaNacimiento;
+	if (edad >= 18) {
+		console.log('este usuario es mayor de edad');
+	} else {
+		console.log('noooo');
+	}  
+
+	}
+	console.log(edad); 
+
+
+
+for (var i = 0; i <= 50; i++) {
+	if(i % 7 == 0){
+	console.log(i * 7);
+	
+}
+ }
